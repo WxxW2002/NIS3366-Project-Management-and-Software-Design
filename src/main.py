3aa762549bb145fdbc32f84c170b059fc75b7bfd
Wang Xin <2364037658@qq.com>
2023-04-22 00:18:20 +0800

    add boot animation

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 # main.py
 import sys
-from PyQt6.QtWidgets import QApplication
+from PyQt6.QtWidgets import QApplication, QSplashScreen
+from PyQt6.QtCore import QTimer, Qt, QRect
+from PyQt6.QtGui import QPixmap
 from ui import MainWindow
 
 from threading import Thread
@@ -11,9 +13,26 @@ def main():
     # Create a QApplication instance
     app = QApplication(sys.argv)
 
+    # Splash screen
+    splash_image = QPixmap('./photos/Hogwarts.jpg')
+    splash_screen = QSplashScreen(splash_image, Qt.WindowType.WindowStaysOnTopHint)
+
+    # Center the splash screen on the screen
+    screen_geometry = app.primaryScreen().geometry()
+    screen_center = screen_geometry.center()
+    splash_screen_center = QRect(0, 0, splash_image.width(), splash_image.height()).center()
+    splash_screen.move(screen_center - splash_screen_center)
+
+    splash_screen.show()   
+
     # Create and display the main application window
     window = MainWindow()
-    window.show()
+
+    # Set timers for splash screen and main window
+    timer = QTimer()
+    timer.singleShot(2000, splash_screen.close)
+    timer.singleShot(2000, window.show)
+
 
     # Create a Notifications instance and start the check_and_notify loop in a new thread
     notifications = Notifications(window.task_list)
